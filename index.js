860a254d6c7fd3fa8d09efd843ab17588cb0acae
Mauiricio Luna <shelldon.luna@gmail.com>
2022-08-03 09:10:07 -0500

    creando el registro de usuarios

--- index.js
+++ index.js
@@ -3,8 +3,39 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const expressJwt = require('express-jwt')
+const User = require('./user')
 
 mongoose.connect('mongodb+srv://shelldon:sh311d0n@cluster0.y8jgm49.mongodb.net/auth?retryWrites=true&w=majority')
-
+//se crea la app con express para utilizar la api hacia mongo
 const app = express()
 app.use(express.json())
+
+
+//se crea la función post la cual  utiliza un try catch en el caso
+//que ya exista el usuario, manda un mensaje de errir 500 en caso de que falle
+app.post('/register', async (req,res) => {
+    const { body } = req
+    console.log({ body })
+    try {
+        const isUser = await User.findOne({ email: body.email })
+        if (isUser) {
+            return res.status(403).send('Usuario ya existe')
+        }
+        //aqui se usa el salt y el hash para encriptar el password
+        //el usuario va a ser el correo electronico
+        const salt  = await bcrypt.genSalt()
+        const hashed = await bcrypt.hash(body.password, salt)
+        const user = await User.create ({email: body.email, password:hashed, salt})
+
+        res.send({ _id: user._id })
+
+    } catch (err) {
+        console.log(err) 
+        res.status(500).send(err.message)
+        
+    }
+})
+
+app.listen(3000, () => {
+    console.log ('Listening por 3000')
+})
--- user.js
+++ user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const User = mongoose.model('User', {
     email: {type: String, required: true},
-    password: {type: String.apply, required: true},
+    password: {type: String, required: true},
     salt: {type: String, required: true},
 })
 
-module.exports = Users
+module.exports = User
